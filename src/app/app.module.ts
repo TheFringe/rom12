b0bc775826954b7ebf0c28c958be2e07d4e10a91
TheFringe <eric.v.lundgren@gmail.com>
2019-07-21 20:49:14 +0200

    flip-card, font-awsome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faThumbsUp, faThumbsDown } from '@fortawesome/free-solid-svg-icons';
+
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, FontAwesomeModule ],
+  declarations: [ AppComponent, HelloComponent, CardComponent ],
   bootstrap:    [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(){
+    library.add(faThumbsDown);
+    library.add(faThumbsUp);
+  }
+}
